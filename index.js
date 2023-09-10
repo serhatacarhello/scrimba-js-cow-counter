5de1f7fb1c6e92d44c6f12f4389db5e08c936fbc
serhatacarhello <serhat.acar.tr@gmail.com>
2023-09-10 08:55:27 +0300

    updated

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById('save-el')
+let countEl = document.getElementById('count-el')
 let count = 0
 
+function updateCountDisplay() {
+  countEl.textContent = count
+}
+
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count++
+  updateCountDisplay()
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+  let countStr = ` ${count} - `
+  saveEl.textContent += countStr
+  count = 0
+  updateCountDisplay() // count = 0
+}
+
+function reset() {
+  saveEl.textContent = ''
 }
